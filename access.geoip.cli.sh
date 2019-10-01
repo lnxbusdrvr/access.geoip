0d1e0dae8a122aedb26099ea0bec5bd56c2d8b63
lnxbusdrvr <anticop@iki.fi>
2019-10-01 19:59:00 +0300

    Add timezone

--- access.geoip.cli.sh
+++ access.geoip.cli.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Get last ip-address from access.log
-for i in $(tail -n 1  /var/log/nginx/access.log | awk '{print $1}');
-# Tell ip-address for API
-# and get result as JSON's city, region and country -infos
-do curl -s http://ipinfo.io/$i | jq -r '.city,.region,.country';
-done
+# CopyPaste this to command line
+for i in $(tail -n 1  /var/log/nginx/access.log | awk '{print $1}'); do curl -s http://ipinfo.io/$i | jq -r '.city,.region,.country,.timezone'; done
